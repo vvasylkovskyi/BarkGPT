4a4edfeab81b8eeccae66e3175afdc1e39596132
Viktor Vasylkovskyi <vvasylkovskyi@pagerduty.com>
2026-03-27 09:16:44 +0000

    Test API Key

--- app/routes/health_check/controller.py
+++ app/routes/health_check/controller.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import APIRouter, status
 from app.http.response import handle_response
 
@@ -6,4 +7,5 @@ health_check_router = APIRouter(prefix="/health-check")
 
 @health_check_router.get("/")
 async def health_check():
-    return handle_response(data={"status": "OK"}, status_code=status.HTTP_200_OK)
+    api_key = os.getenv("API_KEY")
+    return handle_response(data={"status": "OK", "api_key": api_key}, status_code=status.HTTP_200_OK)
