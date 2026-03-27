b10521c4ae3c0cf0086e6d6e8859e61032a127d5
Viktor Vasylkovskyi <vvasylkovskyi@pagerduty.com>
2026-03-27 09:37:31 +0000

    CI/CD with vault works

--- app/routes/health_check/controller.py
+++ app/routes/health_check/controller.py
@@ -1,4 +1,3 @@
-import os
 from fastapi import APIRouter, status
 from app.http.response import handle_response
 
@@ -7,5 +6,4 @@ health_check_router = APIRouter(prefix="/health-check")
 
 @health_check_router.get("/")
 async def health_check():
-    api_key = os.getenv("API_KEY")
-    return handle_response(data={"status": "OK", "api_key": api_key}, status_code=status.HTTP_200_OK)
+    return handle_response(data={"status": "OK"}, status_code=status.HTTP_200_OK)
